a255be954dcd93e82ad65b4cd03f42c8ba8c640a
Svivtik <illusha.kabachny@gmail.com>
2024-06-30 18:50:14 +0200

    Add logging username

--- учебный тг бот.py
+++ учебный тг бот.py
@@ -1,4 +1,5 @@
 import logging
+import settings
 from telegram.ext import Updater, CommandHandler, CallbackContext, MessageHandler, Filters
 
 logging.basicConfig(format='%(asctime)s - %(levelname)s - %(message)s',
@@ -10,6 +11,7 @@ def start_bot(update: Updater, context: CallbackContext):
     mytext = f'''Hello {update.message.chat.first_name}
     
     i have only /start command! =)'''
+    logging.info(f'User {update.message.chat.usenname} press /start')
     update.message.reply_text(mytext)
 
 def chat(update: Updater, context: CallbackContext):
@@ -19,7 +21,7 @@ def chat(update: Updater, context: CallbackContext):
     update.message.reply_text(text)
 
 def main():
-    updtr = Updater('6321694321:AAFErzmd5crdo9VeHTUFp7lNaI-4xwDu6wo')
+    updtr = Updater(settings.TOKEN_TELEGRAM)
 
     updtr.dispatcher.add_handler(CommandHandler('start', start_bot))
     updtr.dispatcher.add_handler(MessageHandler(Filters.text, chat))
